7325410e21bed2615886bedfbcf0e9a3bae0351f
antondurov <antondurov05@protonmail.com>
2026-05-14 09:08:36 -0400

    More py linting fix #2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,14 +28,18 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://u8qdb8t659v-8000.theianext-1-' +
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://u8qdb8t659v-8000.theianext-1-'
     'labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
 ]
-CSRF_TRUSTED_ORIGINS = ['https://u8qdb8t659v-8000.theianext-1-labs-' +
-'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://u8qdb8t659v-8000.theiadockernext-1-labs-prod-' +
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://u8qdb8t659v-8000.theianext-1-labs-'
+    'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://u8qdb8t659v-8000.theiadockernext-1-labs-prod-'
     'theiak8s-4-tor01.proxy.cognitiveclass.ai'
-    ]
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],

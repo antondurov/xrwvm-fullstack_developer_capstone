7590b32b23307c58f6e5884378e74fedb07b53cb
antondurov <antondurov05@protonmail.com>
2026-05-15 07:01:20 -0400

    js linting fix

--- server/database/app.js
+++ server/database/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -33,7 +33,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -60,40 +60,40 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 app.get('/fetchDealers', async (req, res) => {
     try {
         const documents = await Dealerships.find();
-        res.json(documents)
+        res.json(documents);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching documents' })
+        res.status(500).json({ error: 'Error fetching documents' });
     }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
     try {
-        const documents = await Dealerships.find({ state: req.params.state})
-        res.json(documents)
+        const documents = await Dealerships.find({ state: req.params.state});
+        res.json(documents);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching documents'})
+        res.status(500).json({ error: 'Error fetching documents'});
     }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
     try {
-        const documents = await Dealerships.find({ id: req.params.id})
+        const documents = await Dealerships.find({ id: req.params.id});
         if (!documents) {
-            res.status(404).json({ error: "Can't find dealer with provided id"})
+            res.status(404).json({ error: "Can't find dealer with provided id"});
         }
-        res.json(documents)
+        res.json(documents);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching documents'})
+        res.status(500).json({ error: 'Error fetching documents'});
     }
 });
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0]['id'] + 1;
 
   const review = new Reviews({
 		"id": new_id,

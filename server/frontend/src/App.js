d9ab2ee151960788e0ec6e531db361b8f0066317
Pavel Durov <pavel@p3ld3v.dev>
2026-06-07 19:09:18 +0100

    Add catch-all route to redirect to dealers page.

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,5 +1,5 @@
 import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer"
@@ -13,6 +13,7 @@ function App() {
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="*" element={<Navigate to="/dealers" replace />} />
     </Routes>
   );
 }
